6aa78ed4c11685f5e97d54f88d0ba98aab6d67b3
Javier Aguirre <javieraguirre@MacBook-Air-de-Javier.local>
2022-10-11 13:23:29 -0300

    Se agregan cambios relacionados a la extraccion de datos de la POKEAPI en el archivo funciones.js

--- funciones.js
+++ funciones.js
@@ -1,62 +1,62 @@
-// Pokémon de Kanto
+// Pokémon de Johto:
 
 let pokeBoton = document.querySelector('#pokeBoton')
 pokeBoton.addEventListener('click',boton)
+
+let imagenes = {
+    025: 'https://i.imgur.com/ALqmzhO.gif',
+    063: 'https://i.imgur.com/Lmsat0t.gif',
+    133: 'https://i.imgur.com/pUwcrvc.gif',
+    149: 'https://i.imgur.com/ajLI4SC.gif',
+    152: 'https://i.imgur.com/pm3UBbA.gif',
+    155: 'https://i.imgur.com/ISBKDS9.gif',
+    158: 'https://i.imgur.com/KTeT2X6.gif',
+}
 
 let obtenerPokemonApi = async(id) => {
     let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
     let info = await response.json()
     let nombre = info.name
     let nombreMayusculaInicial = nombre[0].toUpperCase() + nombre.slice(1)
-    return nombreMayusculaInicial
+    let stats = info.stats
+    let hpBase = stats.find(e => e.stat.name === 'hp').base_stat
+    let attack = stats.find(e => e.stat.name === 'attack').base_stat
+    let speed = stats.find(e => e.stat.name === 'speed').base_stat
+    let pokemon = new Pokemon(nombreMayusculaInicial, hpBase, imagenes[id],attack,speed)
+    return pokemon
 }
 
 // --- Esta función constructora es para los diferentes pokemon del juego --- //
 
-function Pokemon(nombre, tipo, debilidad, vida, fase, ataque1, ataque1Valor, imageUrl){
-    this.nombre=nombre,
-    this.tipo=tipo,
-    this.debilidad=debilidad,
-    this.vida=vida,
-    this.fase=fase,
-    this.ataque1=ataque1
-    this.ataque1Valor=ataque1Valor
+function Pokemon(name, hp, imageUrl, ataque1Valor, velocidad, tipo, debilidad, fase, ataque1){
+    this.name=name
+    this.hp=hp
     this.imageUrl = imageUrl
+    this.ataque1Valor=ataque1Valor
+    this.velocidad=velocidad
+    // this.tipo=tipo
+    // this.debilidad=debilidad,
+    // this.fase=fase,
+    // this.ataque1=ataque1
 }
-
-let pokemon010 = new Pokemon("Caterpie","Bicho","Volador",85,"Básico","Placaje",20,)
-let pokemon016 = new Pokemon("Pidgey","Volador","Agua",100,"Básico","Ataque alado",35)
-let pokemon025 = new Pokemon("Pikachu","Eléctrico","Lucha",100,"Básico","Impactrueno",35,'https://i.imgur.com/ALqmzhO.gif')
-let pokemon042 = new Pokemon("Zubat","Veneno","Tierra",100,"Básico","Picotazo venenoso",40)
-let pokemon056 = new Pokemon("Mankey","Lucha","Psíquico",100,"Básico","Golpe certero",40)
-let pokemon063 = new Pokemon("Abra","Psíquico","Siniestro",100,"Básico","Confusión",30,'https://i.imgur.com/Lmsat0t.gif')
-let pokemon124 = new Pokemon("Jynx","Hielo","Acero",100,"Básico","Ventisca",30)
-let pokemon132 = new Pokemon("Ditto","Normal","Lucha",85,"Básico","Placaje",20)
-let pokemon133 = new Pokemon("Eevee","Normal","Lucha",100,"Básico","Placaje",20,'https://i.imgur.com/pUwcrvc.gif')
-let pokemon147 = new Pokemon("Dratini","Dragón","Hada",100,"Básico","Garra dragón",45)
-let pokemon149 = new Pokemon("Dragonite","Dragón","Hada",150,"Fase 2","Garra dragón",60,'https://i.imgur.com/ajLI4SC.gif')
-let pokemon152 = new Pokemon("Chikorita","Planta","Fuego",100,"Básico","Hojas navaja",35,"https://i.imgur.com/pm3UBbA.gif")
-let pokemon155 = new Pokemon("Cyndaquil","Fuego","Agua",100,"Básico","Ascuas",35,"https://i.imgur.com/ISBKDS9.gif")
-let pokemon158 = new Pokemon("Totodile","Agua","Eléctrico",100,"Básico","Chorro de agua",35,"https://i.imgur.com/KTeT2X6.gif")
-let equipoPokemon = []
 
 // --- Esta función constructora es para los distintos tipos de contrincantes del juego --- //
 
-function Entrenador(nombreContrincante,equipoContrincante,objetoContrincante,imageUrl){
-    this.nombreContrincante = nombreContrincante,
-    this.equipoContrincante = equipoContrincante,
-    this.objetoContrincante = objetoContrincante,
-    this.imageUrl = imageUrl
-}
+// function Entrenador(nombreContrincante,equipoContrincante,objetoContrincante,imageUrl){
+//     this.nombreContrincante = nombreContrincante,
+//     this.equipoContrincante = equipoContrincante,
+//     this.objetoContrincante = objetoContrincante,
+//     this.imageUrl = imageUrl
+// }
 
-const red = new Entrenador("Red",pokemon025,undefined,'https://i.imgur.com/ELBXnic.gif')
-const serena = new Entrenador("Serena",pokemon063,undefined,'https://i.imgur.com/6McxONW.gif')
-const oak = new Entrenador("Oak",pokemon124,undefined,'https://i.imgur.com/WJOAs50.gif')
-const gary = new Entrenador("Gary",pokemon133,undefined,'https://i.imgur.com/l2Dx95N.gif')
+// const red = new Entrenador("Red",pokemon025,undefined,'https://i.imgur.com/ELBXnic.gif')
+// const serena = new Entrenador("Serena",pokemon063,undefined,'https://i.imgur.com/6McxONW.gif')
+// const oak = new Entrenador("Oak",pokemon124,undefined,'https://i.imgur.com/WJOAs50.gif')
+// const gary = new Entrenador("Gary",pokemon133,undefined,'https://i.imgur.com/l2Dx95N.gif')
 
 async function desplegarAlertaPokemonInicial(pokemon){
     await Swal.fire({
-        text: `¡Felicidades! Ahora te acompaña ${pokemon.nombre} y tiene ${pokemon.vida} puntos de salud`,
+        text: `¡Felicidades! Ahora te acompaña ${pokemon.name}. Tiene ${pokemon.hp} puntos de salud y ${pokemon.velocidad} puntos de velocidad.`,
         imageUrl: pokemon.imageUrl,
         imageWidth: 400,
         imageHeight: 200,
@@ -130,7 +130,7 @@ async function boton(){
         confirmButtonText:'¡Yo te elijo!',
         allowOutsideClick: false,
         input: 'radio',
-        inputOptions: {Cyndaquil: await obtenerPokemonApi(155), Totodile: await obtenerPokemonApi(158), Chikorita: await obtenerPokemonApi(152)},
+        inputOptions: {Cyndaquil: await (await obtenerPokemonApi(155)).name, Totodile: await (await obtenerPokemonApi(158)).name, Chikorita: await (await obtenerPokemonApi(152)).name},
         inputValidator: (value) => {
           if (!value) {
             return '¡Necesitas elegir uno!'
@@ -141,11 +141,11 @@ async function boton(){
     console.log(segundoAlert)
 
     if(eleccionInicial === 'Cyndaquil'){
-        await desplegarAlertaPokemonInicial(pokemon155)
+        await desplegarAlertaPokemonInicial(await obtenerPokemonApi(155))
     } else if(eleccionInicial==='Totodile'){
-        await desplegarAlertaPokemonInicial(pokemon158)
+        await desplegarAlertaPokemonInicial(await obtenerPokemonApi(158))
     } else{
-        await desplegarAlertaPokemonInicial(pokemon152)
+        await desplegarAlertaPokemonInicial(await obtenerPokemonApi(152))
     }
 
     await Swal.fire({
